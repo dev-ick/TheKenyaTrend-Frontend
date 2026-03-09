a26b792772c4907e3966f3e5b1b669445f8c116e
dev-ick <165423990+dev-ick@users.noreply.github.com>
2026-03-09 09:22:22 +0300

    Update MainPost.tsx
    This change is supposed to resolve the error of no props in redeployment failure

--- MainPost.tsx
+++ MainPost.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import './MainPost.css';
 
-const MainPost = ({ post }) => {
+type Post = {
+  title: string
+  content?: string
+  image?: string
+}
+
+type Props = {
+  post: Post
+}
+
+const MainPost = ({ post }: Props) => {
     return (
         <div className="main-post">
             <h1>{post.title}</h1>
